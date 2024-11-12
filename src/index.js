20256503aa4fc0a463c36bd1e2d823a5a136cc8d
friveratobar <friveratobar@gmail.com>
2024-11-12 19:43:13 -0300

    primera subida

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Booklist(){
+ return (
+    <section>
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+    </section>
+ );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const Book = () =>{
+    return(
+
+        <article>
+            <Image  />
+            <Title  />
+            <Author />
+        </article>
+    );
+  }
+
+  const Image = () => <h2> Image placeholder </h2>;
+
+  const Title = () => {
+    return <h2> Book Title </h2>;
+  }
+
+  const Author = () => <h4> Author </h4>;
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+
+  root.render(<Booklist/>);
